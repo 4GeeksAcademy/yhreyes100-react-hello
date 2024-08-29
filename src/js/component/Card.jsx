640e32fc5ab850b126002cc2a5d4a0670d578453
Yasin <37377965+yhreyes100@users.noreply.github.com>
2024-08-29 02:55:34 +0000

    commit

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -3,6 +3,7 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 const Cards=(props)=>{
 	return (
+		
 		<div className="col-lg-3 col-md-6 col-sm-12" > 
 		<Card style={{ width: '18rem' }}>
 		<Card.Img variant="top" src={props.imgUrl} />
